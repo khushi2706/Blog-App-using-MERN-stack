d11ca9003527756f7b0e00212b75d45887cd272a
khushi patel <khushi@socialpilot.co>
2023-10-01 13:03:13 +0530

    Revert "Add a route in userroute"
    This reverts commit 5a8aa3a813bfa0a45e0310f9b1a1411acdb147b2.

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -2,9 +2,7 @@ const express = require("express");
 const {  getAllUser , signUp ,logIn } = require("../controller/user-contoller");
 const userRouter = express.Router();
 
-userRouter.get("/khushi",(req,res)=>{
-    res.send(" you are awesome can we collaborative for mernstack project i have yotube channel called simplyjs you can checkout and try to work together");
-})
+
 userRouter.get("/",getAllUser);
 userRouter.post("/signup", signUp);
 userRouter.post("/login" , logIn);
