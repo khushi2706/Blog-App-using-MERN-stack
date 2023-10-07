b8f8aba9429933f8f66c264798e99cc1490b014d
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-07 11:17:42 -0700

    added the styled-components package

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
---
+++ client/src/utils/theme.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-labels */
+export const darkTheme = {
+    bg: '#181818',
+    bgLi : '#202020',
+    text : 'white',
+    textSof : '#aaaaaa'
+};
+
+export const lightTheme = {
+    bg: '#ebebeb',
+    bgLi : 'white',
+    text : 'black',
+    textSof : '#606060'
+};
