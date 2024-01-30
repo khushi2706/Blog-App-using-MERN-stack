520feeac2cf753e9947469bbf0049a4d0c9e5b24
younes1337 <raymondyounes2@gmail.com>
2024-01-30 17:36:05 +0100

    Commit Message: Update SignUp component to handle user sign-up and display Snackbar messages
    Description:
    - Modified the SignUp component to handle user sign-up functionality.
    - Implemented asynchronous form submission handling in the handleSubmit function.
    - Added error handling using try-catch blocks to catch errors during sign-up.
    - Updated the Snackbar messages to provide feedback to the user based on the sign-up status.
    - Snackbar messages now display success message for successful sign-up and error message for failed sign-up attempts.
    - If the error response indicates that the email already exists, the Snackbar displays a message informing the user that the email is already in use.
    - Implemented efficient error handling to provide clear feedback to the user.

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
 
 
-mongoose.connect("Enter you MongoDb Uri").then(()=>{
+mongoose.connect("Enter you MongoDb Uri Here").then(()=>{
     console.log("DataBase Is connected Now Let's Store Some Data..!");
 }).catch((err)=>{
     console.log(err);
